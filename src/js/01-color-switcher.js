d421285a7c71b807d2223d350beb5f102abc970a
SergiyRybin <serhiobax@gmail.com>
2022-02-20 10:10:48 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,17 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
 
+
+const bodyBacground = document.querySelector("body")
+const starBatton = document.querySelector("button[data-start]")
+const stopBatton = document.querySelector("button[data-stop]")
+
+// console.log(bodyBacground.style.display = "flex")
+// console.log(bodyBacground.style)
+
+starBatton.addEventListener("click", changeColor)
+
+function changeColor(){
+    bodyBacground.style.backgroundColor = getRandomHexColor()
+}
