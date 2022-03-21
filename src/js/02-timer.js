decb5cb07b0f0a4d8cda7c6787607bd806b68406
SergiyRybin <serhiobax@gmail.com>
2022-03-21 15:07:38 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,16 +3,16 @@ import "flatpickr/dist/flatpickr.min.css";
 import Notiflix from 'notiflix';
 
 const startButton = document.querySelector("button[data-start]")
-// startButton.disabled=true
-
 const timerDay = document.querySelector("[data-days]")
 const timerHouers = document.querySelector("[data-hours]")
 const timerMinutes = document.querySelector("[data-minutes]")
 const timerSeconds = document.querySelector("[data-seconds]")
 
+startButton.disabled=true
 
 
 let msi = null
+let selected = null
 const options = {
     enableTime: true,
     time_24hr: true,
@@ -20,37 +20,40 @@ const options = {
     minuteIncrement: 1,
     onClose(selectedDates) {
         if(options.defaultDate >= selectedDates[0]){
-
             Notiflix.Notify.failure("Please choose a date in the future")
-            // startButton.disabled=true
-
-        }else{
-            // startButton.disabled=false
+            startButton.disabled=true
         }
-    //   console.log(selectedDates[0].getTime() - options.defaultDate.getTime());
-    //   console.log(options.defaultDate.getTime())
-    msi = selectedDates[0]-options.defaultDate
+        startButton.disabled=false
 
-    timerDay.textContent = convertMs(msi).days 
-    timerHouers.textContent = convertMs(msi).hours
-    timerMinutes.textContent = convertMs(msi).minutes
-    timerSeconds.textContent = convertMs(msi).seconds
+        selected = selectedDates[0]
+        msi = selected-options.defaultDate
+        timerDay.innerHTML = convertMs(msi).days 
+        timerHouers.innerHTML = convertMs(msi).hours
+        timerMinutes.innerHTML = convertMs(msi).minutes
+        timerSeconds.innerHTML = convertMs(msi).seconds
+
 
 },
   };
-
-
 flatpickr("#datetime-picker", options)
 
-startButton.addEventListener("click",()=>{
-    setInterval(()=>{
-   if(timerSeconds.innerHTML >0 || timerSeconds <=60){
-    console.log(timerSeconds.innerHTML-=1)
-    return
-    
-   } 
 
+startButton.addEventListener("click",()=>{
+    startButton.disabled=true
+
+    setInterval(()=>{
+        let now =  new Date()
+        msi = selected-now
+        console.log(msi)
+        timerDay.innerHTML = convertMs(msi).days 
+        timerHouers.innerHTML = convertMs(msi).hours
+        timerMinutes.innerHTML = convertMs(msi).minutes
+        timerSeconds.innerHTML = convertMs(msi).seconds
+        if(msi<0){
+           Notiflix.Notify.failure("Please stop") 
+        }
     }, 1000)
+   
 
 })
 
